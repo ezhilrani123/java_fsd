e413dab8783606883a01e8736de1c092771f71b5
ezhilrani123 <109669154+ezhilrani123@users.noreply.github.com>
2022-08-01 13:24:15 +0530

    Update TypeCasting.java

--- Assisted_Project/TypeCasting.java
+++ Assisted_Project/TypeCasting.java
@@ -1,1 +1,20 @@
+package assisted_project;
+
+import java.util.Scanner;
+
+public class TypeCasting {
+           public static void main(String[] args) {
+			int num1;
+			double num2;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter a first number");
+			num1=sc.nextInt();
+			System.out.println("Enter a second numbter");
+			num2=sc.nextDouble();
+			double ans1=num1;//implicit type casting
+			int ans2=(int)num2;//explicit type casting
+			System.out.println(ans1+" "+ans2);
+		}
+           
+}
 
